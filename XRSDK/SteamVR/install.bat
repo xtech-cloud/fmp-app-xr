7b7655cc7c19a64311f552ebb4213d1edfc1c6d9
Niall Liu <easlee@outlook.com>
2023-03-12 18:51:06 +0800

    update

---
+++ XRSDK/SteamVR/install.bat
@@ -0,0 +1,5 @@
+@echo off
+xcopy /E/Y/Q 2.7.3\Assets ..\..\FMP\Assets\
+xcopy /E/Y/Q 2.7.3\Packages ..\..\FMP\Packages\
+xcopy /E/Y/Q 2.7.3\ProjectSettings ..\..\FMP\ProjectSettings\
+pause
---
+++ XRSDK/SteamVR/uninstall.bat
@@ -0,0 +1,12 @@
+@echo off
+DEL /S/Q ..\..\FMP\Assets\SteamVR
+DEL /S/Q ..\..\FMP\Assets\SteamVR.meta
+DEL /S/Q ..\..\FMP\Assets\SteamVR_Resources
+DEL /S/Q ..\..\FMP\Assets\SteamVR_Resources.meta
+DEL /S/Q ..\..\FMP\Assets\StreamingAssets
+DEL /S/Q ..\..\FMP\Assets\StreamingAssets.meta
+DEL /S/Q ..\..\FMP\Assets\XR\Loaders
+DEL /S/Q ..\..\FMP\Assets\XR\Loaders.meta
+DEL /S/Q ..\..\FMP\Assets\XR\Settings
+DEL /S/Q ..\..\FMP\Assets\XR\Settings.meta
+pause
